ac2dcb109afb5d30d0de1c022665977d93e39b2a
mea <shoto.dragnir@gmail.com>
2026-03-30 11:35:13 +0200

    fix: (mais ça marche pas)
    Co-authored-by: TOBLADZ <TOBLADZ@users.noreply.github.com>

--- src/prototypePy/main.py
+++ src/prototypePy/main.py
@@ -24,27 +24,66 @@ def moyenne_des_differences(individu1, individu2):
         val1 = individu1[k] if (isinstance(individu1, dict)) else individu1.genome.get_val(k)
         val2 = individu2[k] if (isinstance(individu2, dict)) else individu2.genome.get_val(k)
 
-        nb_alleles_etudiees += 1 if (type_allele == "int" or "list") else 0
-
         if type_allele == "int":
             somme = val1 + val2
             if somme != 0:
-                # diff relative = |a - b| / moyenne(a, b)
+                    # diff relative = |a - b| / moyenne(a, b)
                 total_differences += abs((val1 - val2) / (somme / 2))
+                nb_alleles_etudiees += 1
         
         elif type_allele == "list":
-            diff = 0
+            diff, elements_valides = 0, 0
             for i in range( len(val1) ):
                 somme = val1[i] + val2[i]
                 if somme != 0:
                     diff += abs((val1[i] - val2[i]) / (somme / 2))
+                    elements_valides += 1
             
-            # diffR total = moyenne des differences relatives
-            total_differences += diff / len(val1)
+            if elements_valides > 0:
+                total_differences += diff / elements_valides
+                nb_alleles_etudiees += 1
     
+    if nb_alleles_etudiees == 0:
+        return 0
 
     return total_differences / nb_alleles_etudiees
 
+
+def agentArchetype(popu):
+    taille_pop = len(popu)
+    archetype = {} 
+    for agent in popu:
+        for k in dico_alleles.keys():
+            type_allele = dico_alleles[k][5]
+            val = agent.genome.get_val(k)
+
+            if type_allele == "int":
+                if k not in archetype:
+                    archetype[k] = val
+                else:
+                    archetype[k] += val
+
+            elif type_allele == "list":
+                if k not in archetype:
+                    archetype[k] = list(val) 
+                else:
+                    for i in range(len(archetype[k])):
+                        archetype[k][i] += val[i]
+
+            if type_allele == "str":
+                archetype[k] = val
+    
+    for k in archetype.keys(): # division par le nombre d'invidivus
+        type_allele = dico_alleles[k][5]
+
+        if type_allele == "int":
+            archetype[k] /= taille_pop
+
+        elif type_allele == "list":
+            for i in range(len(archetype[k])):
+                archetype[k][i] /= taille_pop
+    
+    return archetype
 
 def NouvelleEspecePointDInterrogation(popu):
     """!
@@ -56,64 +95,50 @@ def NouvelleEspecePointDInterrogation(popu):
     if taille_pop < 5 : # pas besoin de réfléchir si y a pas assez d'indiv
         return None
     
-    archetype = {} # création du profil archetype
-    for agent in popu:
-        for k in dico_alleles.keys():
-            type_allele = dico_alleles[k][5]
+    #   PROFIL ARCHÉTYPE
+    archetype = dict(agentArchetype(popu))
 
-            if type_allele == "int":
-                if k not in archetype :
-                    archetype[k] = agent.genome.get_val(k)  
-                else:
-                    archetype[k] += agent.genome.get_val(k)
-
-            if type_allele == "list":
-                if k not in archetype :
-                    archetype[k] = agent.genome.get_val(k)  
-                else:
-                    for i in range(len(archetype[k])):
-                        archetype[k][i] += agent.genome.get_val(k)[i]
-
-            if type_allele == "str":
-                archetype[k] = agent.genome.get_val(k)
-    
-    for a in archetype.values(): # division par le nombre d'invidivus
-        if isinstance(a, int) :
-            a /= taille_pop
-        
-        if isinstance(a, list) :
-            for i in range( len(a) ):
-                a[i] /= taille_pop
-
-        print(a)
     ##
 
-    agent_eloigne = None # On trouve l'agent le "plus mutant"
-    m = 0
+    agent_eloigne = None 
+    max_distance = -1 
+    
     for agent in popu:
-        m2 = moyenne_des_differences(agent,archetype)
-        if m<=m2:
-            m = m2
+        m2 = moyenne_des_differences(agent, archetype)
+        if m2 > max_distance:
+            max_distance = m2
             agent_eloigne = agent
     
-    #On compte combien d'agents sont similaires et différents par rapport à l'agent le plus éloigné pour savoir si on crée un nouvelle espèce
+    # si le + grand mutant est quasi la moyenne -> on annule
+    if max_distance < 0.15:
+        return None
+
     differents = 0
     similaires = 0
     for agent in popu:
-        if agent != agent_eloigne:
-            distance = moyenne_des_differences(agent_eloigne,agent)
+        # l'agent compte dans sa famille maintenant
+        distance = moyenne_des_differences(agent_eloigne, agent)
 
-            if distance <= 0.2:
-                similaires += 1
-            elif distance > 0.3:
-                differents += 1
+        if distance <= 0.15:
+            similaires += 1
+        elif distance > 0.25:
+            differents += 1
 
-    # si sous-groupe (similaires) + éloigné de la masse (différents)
-    if similaires >= taille_pop/4 and differents >= taille_pop / 2:
+    # si sous-groupe (10% mini) + éloigné de la masse (30% mini)
+    if similaires >= taille_pop *0.205 and  differents >= taille_pop *0.35:
         return agent_eloigne
 
+    if moyenne_des_differences(archetype,suivi_espece[popu[0].id_espece][3]) >= 0.1:
+        distance = 1
+        agent_proche = None
+        for agent in popu:
+            m = moyenne_des_differences(agent,archetype)
+            if m < distance:
+                agent_proche = agent
+                distance = m
+        return agent_proche
+    
     return None
-
 
 #Initialisation de Pygame
 pygame.init()
@@ -161,29 +186,46 @@ world.procedural_generation()
 
 #Création de grenouille-----------------------------------------------------------------
 grenouilles = []
+grenouilles_bis = []
 
-for i in range(5):
+for i in range(10):
     valide = False
     
     while not valide:
         x, y = randint(0, W), randint(0, H)
         if allowed_mask.get_at((x, y)):
             valide = True
-            
-    grenouilles.append(Individu(x, y, 0))
+    
+    if i > 5:       
+        grenouilles.append(Individu(x, y, 0))
+    else:
+        grenouilles_bis.append(Individu(x,y,1))
 
 for g in grenouilles:
+        g.craft_individu()
+        g.give_rect(g.genome.get_val("taille"))
+        Population.add_individu(g)
+for g in grenouilles_bis:
         g.craft_individu()
         g.give_rect(g.genome.get_val("taille"))
         Population.add_individu(g)
 
 
 grenouille = Espece(0,0)
+grenouille_bis = Espece(1,0)
 
-liste_especes = [grenouille]
-#   ID de l'espèce : [ID du parent, Année de naissance, Année de mort]
-suivi_espece = {0 : [None, 0, None]}
-#--------------------------------------------------------------------------------------- 
+liste_especes = [grenouille,grenouille_bis]
+#   ID de l'espèce : [ID du parent, Année de naissance, Année de mort, agent archétype à la création]
+suivi_espece = {0 : [None, 0, None], 1 : [None,0,None]}
+#---------------------------------------------------------------------------------------
+popu1, popu2 = [], []
+for p in Population.populations:
+    if p.id_espece == 0:
+        popu1.append(p)
+    else:
+        popu2.append(p)
+suivi_espece[0].append(agentArchetype(popu1)) 
+suivi_espece[1].append(agentArchetype(popu2))
 
 paused = False        
 running = True
@@ -250,9 +292,25 @@ while running:
                     liste_especes.append(a)
                     suivi_espece[liste_especes[-1].id_espece] = [agent.id_espece, age, None]
                     agent.id_espece = liste_especes[-1].id_espece
+                    popu_bis = []
                     for indiv in popu:
-                        if moyenne_des_differences(agent,indiv) <= 0.2 and agent != indiv:
+                        if moyenne_des_differences(agent,indiv) <= 0.15 and agent != indiv:
                             indiv.id_espece = liste_especes[-1].id_espece
+                            popu_bis.append(indiv)
+                    suivi_espece[liste_especes[-1].id_espece].append(agentArchetype(popu_bis))
+                    
+                    popu_bis_bis = []
+                    for indiv in popu:
+                        if indiv.id_espece == suivi_espece[liste_especes[-1].id_espece][0]:
+                            popu_bis_bis.append(indiv)
+                    suivi_espece[suivi_espece[liste_especes[-1].id_espece][0]][3] = dict(agentArchetype(popu_bis_bis))
+
+                    print("Changement : ")
+                    for i in range(len(suivi_espece.keys())):
+                        print(i," : ", end = "")
+                        for I in range(3):
+                            print(suivi_espece[i][I],end=" ")
+                        print("")
 
                 
                 e.update()
@@ -261,6 +319,7 @@ while running:
                 if len(e.effectif)>1 and e.effectif[-1] == 0 and not e.morte:
                     e.morte = True
                     suivi_espece[e.id_espece][2] = age
+                    print(f"Changement : {suivi_espece}")
 
 
         age_texte = font.render(f"Années : {round(age/60, 1)}", True, (255, 255, 255))
